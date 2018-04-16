f27a6bffac6f6b0897c98f0a0a5ceade9a21c360
YHGui <yhgui2014@gmail.com>
2018-04-16 14:16:02 +0800

    week8

---
+++ example/Lab07/Link.py
@@ -0,0 +1,31 @@
+class Link():
+	"""Link class"""
+	empty = ()
+
+	def __init__(self, first, rest = empty):
+		assert rest is Link.empty or isinstance(rest, Link)
+		self.first = first
+		self.rest = rest
+
+	def __getitem__(self, i):
+		if i == 0:
+			return self.first
+		else:
+			return self.first[i - 1]
+	def __len__(self):
+		return 1 + len(self.rest)
+
+	def __repr__(self):
+		if self.rest:
+			rest_str = ', ' + repr(self.rest)
+		else:
+			rest_str = ''
+		return 'Link({0}{1})'.format(self.first, rest_str)
+
+	@property
+	def second(self):
+		return self.rest.first
+	
+	@second.setter
+	def second(self, value):
+		self.rest.first = value	
