6c0e340024b7f0860a8148ebc68538c5fdd61899
YHGui <yhgui2014@gmail.com>
2018-04-27 18:28:47 +0800

    hw10

---
+++ hw/hw10/hw10.scm
@@ -0,0 +1,18 @@
+(define (accumulate combiner start n term)
+  (if (= n 0)
+      start
+      (accumulate combiner (combiner start (term n)) (- n 1) term)
+  )
+)
+
+(define (accumulate-tail combiner start n term)
+  (if (= n 0)
+      start
+      (accumulate-tail combiner (combiner start (term n)) (- n 1) term)
+  )
+)
+
+(define-macro (list-of expr for var in seq if filter-fn)
+  `(map (lambda (,var) ,expr) 
+  	(filter (lambda (,var) ,filter-fn) ,seq))
+)
